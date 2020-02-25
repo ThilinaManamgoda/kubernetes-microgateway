3fd6ce26e4b9a600f7e09a66fc9a1f24f5cf6d12
maanadev <maanafunedu@gmail.com>
2020-02-25 22:32:07 +0530

    Add secrets

--- helm/microgateway/logging/init.sh
+++ helm/microgateway/logging/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-touch /home/ballerina/logs/microgw.log
-/ballerina/runtime/bin/ballerina run --config /home/ballerina/conf/micro-gw.conf $PROJECT.balx 2>&1 | tee -a /home/ballerina/logs/microgw.log
+touch /home/ballerina/wso2/logs/microgw.log
+ballerina run --config /home/ballerina/conf/micro-gw.conf $PROJECT.balx 2>&1 | tee -a /home/ballerina/wso2/logs/microgw.log
